897ab9b60fcd77e71a6697e066a0ad4f7bba7b48
max <max@suedien.de>
2020-05-07 10:39:22 +0200

    added components

---
+++ src/components/Dropdown.vue
@@ -0,0 +1,43 @@
+<template>
+  <select name="wochentag" id="select">
+    <option>Montag</option>
+    <option>Dienstag</option>
+    <option>Mittwoch</option>
+    <option>Donnerstag</option>
+    <option>Freitag</option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: 'Dropdown',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#select {
+  position: absolute;
+  font-family: inherit;
+  font-size: 24px; 
+  padding: 5px;
+  background-color: white;
+  border: 1px rgb(96, 96, 96) solid; 
+  right: 4.6vw;
+  top: 75px;
+} 
+
+#select:hover {
+  background-color: lightgray;
+}
+
+
+option {
+  background-color: white;
+  padding-top: 5px;
+  padding-bottom: 5px;
+}
+</style>
--- src/views/Plan.vue
+++ src/views/Plan.vue
@@ -1,75 +1,25 @@
 <template>
 <div id="body">
     <div id="container">
-  <div id="heading">
-  <h2>HfG <br> Mensakarte</h2>
-    <select name="wochentag" id="select">
-      <option>Montag</option>
-      <option>Dienstag</option>
-      <option>Mittwoch</option>
-      <option>Donnerstag</option>
-      <option>Freitag</option>
-    </select>
-  </div>
-
-  <table id="table">
-    <tr>
-      <th id="leftRow">Kategorie</th>
-      <th id="headerRow">Bezeichnung</th>
-      <th id="headerRow">Inhalte</th>
-      <th id="headerRow">Labels</th>
-      <th id="headerRow">Preis</th>
-    </tr>
-    <tr>
-      <th id="leftRow">Suppe</th>
-    </tr>
-    <tr>
-      <th id="leftRow">Hauptgericht 1</th>
-    </tr>
-    <tr>
-      <th id="leftRow">Hauptgericht 2</th>
-    </tr>
-    <tr>
-      <th id="leftRow">Hauptgericht 3</th>
-    </tr>
-    <tr>
-      <th id="leftRow">Hauptgericht 4</th>
-    </tr>
-    <tr>
-      <th id="leftRow">Beilage 1</th>
-    </tr>
-    <tr>
-      <th id="leftRow">Beilage 2</th>
-    </tr>
-    <tr>
-      <th id="leftRow">Beilage 3</th>
-    </tr>
-    <tr>
-      <th id="leftRow">Beilage 4</th>
-    </tr>
-    <tr>
-      <th id="leftRow">Beilage 5</th>
-    </tr>
-    <tr>
-      <th id="leftRow">Beilage 6</th>
-    </tr>
-    <tr>
-      <th id="leftRow">Nachtisch 1</th>
-    </tr>
-    <tr>
-      <th id="leftRow">Nachtisch 2</th>
-    </tr>
-    <tr>
-      <th id="leftRow">Nachtisch 3</th>
-    </tr>
-  </table>
-  </div>
+      <div id="heading">
+        <h2>HfG <br> Mensakarte</h2>
+      </div>
+      <dropdown></dropdown>
+    <list></list>
+    </div>
 </div>
 </template>
-
 <script>
+import List from '../components/List.vue'
+import Dropdown from '../components/Dropdown.vue'
+
+
 export default {
   name: 'Plan',
+  components: {
+    Dropdown,
+    List
+  },
   props: {
     msg: String
   }
@@ -78,21 +28,13 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-#table {
-  width: 90vw;
-  margin: auto;
-  margin-top: 18vh;
-  text-align: left;
-  padding: 5px, 5px, 5px, 5px;
-  border: 1px black solid;
-}
-
 #heading {
   float: left;
   height: 50px;
 }
 
 #body { 
+  padding: 20px;
   background-color: beige;
   font-family: Futura, "Trebuchet MS", Arial, sans-serif; 
   font-size: 28px; 
@@ -103,7 +45,7 @@ export default {
 }
 
 #container {
-  background-color: beige;
+  background-color: white;
   font-family: Futura, "Trebuchet MS", Arial, sans-serif; 
   font-size: 28px; 
   font-style: normal; 
@@ -116,43 +58,5 @@ export default {
   padding-left: 40px;
   padding-top: 15px;
   padding-right: 40px;
-}
-
-#select {
-  position: absolute;
-  font-family: inherit;
-  font-size: 24px; 
-  padding: 5px;
-  background-color: white;
-  border: 1px rgb(96, 96, 96) solid; 
-  right: 4.6vw;
-  top: 75px;
-} 
-
-#select:hover {
-  background-color: lightgray;
-}
-
-option {
-  background-color: white;
-  padding-top: 5px;
-  padding-bottom: 5px;
-}
-
-#headerRow {
-  width: 350px;
-}
-
-#leftRow {
-  width: 250px;
-}
-
-th {
-  padding-top: 10px;
-  padding-bottom: 10px;
-  font-family: inherit;
-  font-size: 24px;
-  font-weight: bold;
-  padding-left: 10px;
 }
 </style>
